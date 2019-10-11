907335c41741de02d52137d0c8264057288ddb45
Alexey Semchenko <asemchenko@outlook.com>
2019-10-11 16:12:20 +0300

    Update autogrowth script

--- autogrowth_setting.sql
+++ autogrowth_setting.sql
@@ -5,6 +5,8 @@ GO
 DECLARE @rows_autogrowth_size_mb int = 512
        ,@logs_autogrowth_size_mb int = 256;
 SELECT
+    DB_NAME(database_id) as [database_name],
+    [name] as [file_name],
     N'ALTER DATABASE [' + DB_NAME(database_id) + N'] MODIFY FILE ( NAME = N''' + [name]
     + N''', FILEGROWTH = ' + CAST(@rows_autogrowth_size_mb AS nvarchar(50)) + N'MB' + N' );' AS modify_file_sql
 FROM
@@ -15,6 +17,8 @@ WHERE
     AND growth < @rows_autogrowth_size_mb / 8 * 1024
 UNION ALL
 SELECT
+    DB_NAME(database_id) as [database_name],
+    [name] as [file_name],
     N'ALTER DATABASE [' + DB_NAME(database_id) + N'] MODIFY FILE ( NAME = N''' + [name]
     + N''', FILEGROWTH = ' + CAST(@logs_autogrowth_size_mb AS nvarchar(50)) + N'MB' + N' );' AS modify_file_sql
 FROM
@@ -22,4 +26,5 @@ FROM
 WHERE
     database_id > 4
     AND [type] = 1
-    AND growth < @logs_autogrowth_size_mb / 8 * 1024;
+    AND growth < @logs_autogrowth_size_mb / 8 * 1024
+ORDER BY [database_name], [name];
