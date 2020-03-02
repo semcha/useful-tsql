11529880cbb4b450c26d4729d4bf0ce416a6ae6c
Alexey Semchenko <asemchenko@outlook.com>
2020-03-02 18:48:40 +0300

    Update formatting

--- sqlmon.sql
+++ sqlmon.sql
@@ -1,14 +1,14 @@
 -- Монторинг выполняемых запросов
 SELECT
-	es.session_id AS [sid]
-   ,er.blocking_session_id AS blocking_sid
+    es.session_id AS [sid]
+   ,er.blocking_session_id AS bloc_sid
    ,CAST(er.total_elapsed_time / 1000. / 60. AS DECIMAL(34, 2)) AS [duration]
-   ,DB_NAME(er.database_id) AS [database_name]
+   ,DB_NAME(er.database_id) AS [db_name]
    ,SUBSTRING(qt.[text], (er.statement_start_offset / 2) + 1,
-	((CASE
-		WHEN er.statement_end_offset = -1 THEN LEN(CONVERT(NVARCHAR(MAX), qt.[text])) * 2
-		ELSE er.statement_end_offset
-	END - er.statement_start_offset) / 2) + 1) AS [statement]
+    ((CASE
+        WHEN er.statement_end_offset = -1 THEN LEN(CONVERT(NVARCHAR(MAX), qt.[text])) * 2
+        ELSE er.statement_end_offset
+    END - er.statement_start_offset) / 2) + 1) AS [statement]
    ,qt.[text] AS query
    ,qt.objectid
    ,er.[status]
@@ -17,7 +17,7 @@ SELECT
    ,es.[host_name]
    ,es.[program_name]
    ,er.start_time
-	--,--qp.query_plan
+    --,qp.query_plan
    ,er.wait_type
    ,er.cpu_time
    ,er.logical_reads
@@ -27,15 +27,15 @@ SELECT
    ,mg.requested_memory_kb / 1024 AS requested_memory_mb
    ,mg.granted_memory_kb / 1024 AS granted_memory_mb
 FROM
-	sys.dm_exec_requests AS er WITH (NOLOCK)
-	INNER JOIN sys.dm_exec_sessions AS es WITH (NOLOCK)
-		ON er.session_id = es.session_id
-	LEFT JOIN sys.dm_exec_query_memory_grants AS mg WITH (NOLOCK)
-		ON es.session_id = mg.session_id
-	CROSS APPLY sys.dm_exec_sql_text(er.[sql_handle]) AS qt
-	--OUTER APPLY sys.dm_exec_query_plan(er.plan_handle) AS qp WITH (NOLOCK)
+    sys.dm_exec_requests AS er WITH (NOLOCK)
+    INNER JOIN sys.dm_exec_sessions AS es WITH (NOLOCK)
+        ON er.session_id = es.session_id
+    LEFT JOIN sys.dm_exec_query_memory_grants AS mg WITH (NOLOCK)
+        ON es.session_id = mg.session_id
+    CROSS APPLY sys.dm_exec_sql_text(er.[sql_handle]) AS qt
+--OUTER APPLY sys.dm_exec_query_plan(er.plan_handle) AS qp
 WHERE
-	es.is_user_process = 1
-	AND es.session_id NOT IN (@@spid)
+    es.is_user_process = 1
+    AND es.session_id NOT IN (@@spid)
 ORDER BY [statement], er.session_id DESC
 OPTION (RECOMPILE);
