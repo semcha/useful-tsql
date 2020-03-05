a1dc72e05aa00908c54e395e0eee3dc85dbf652d
Alexey Semchenko <asemchenko@outlook.com>
2020-03-05 16:52:39 +0300

    Chage ExtEvnt duration filter to 1 minute

--- extended_events_session.sql
+++ extended_events_session.sql
@@ -9,16 +9,16 @@ ADD EVENT sqlserver.existing_connection(
     ACTION(sqlserver.query_hash,sqlserver.sql_text,sqlserver.client_hostname,sqlserver.client_app_name,sqlserver.database_name,sqlserver.nt_username,sqlserver.username,sqlserver.server_principal_name)),
 ADD EVENT sqlserver.rpc_completed(
     ACTION(sqlserver.query_hash,sqlserver.sql_text,sqlserver.client_hostname,sqlserver.client_app_name,sqlserver.database_name,sqlserver.nt_username,sqlserver.username,sqlserver.server_principal_name)
-    WHERE ([duration]>=(120000000) AND ([package0].[greater_than_uint64]([sqlserver].[database_id],(4))) AND ([sqlserver].[is_system]=(0)))),
+    WHERE ([duration]>=(60000000) AND ([package0].[greater_than_uint64]([sqlserver].[database_id],(4))) AND ([sqlserver].[is_system]=(0)))),
 ADD EVENT sqlserver.sp_statement_completed(SET collect_object_name=(1)
     ACTION(sqlserver.query_hash,sqlserver.sql_text,sqlserver.client_hostname,sqlserver.client_app_name,sqlserver.database_name,sqlserver.nt_username,sqlserver.username,sqlserver.server_principal_name)
-    WHERE ([duration]>=(120000000) AND ([package0].[greater_than_uint64]([sqlserver].[database_id],(4))) AND ([sqlserver].[is_system]=(0)))),
+    WHERE ([duration]>=(60000000) AND ([package0].[greater_than_uint64]([sqlserver].[database_id],(4))) AND ([sqlserver].[is_system]=(0)))),
 ADD EVENT sqlserver.sql_batch_completed(
     ACTION(sqlserver.query_hash,sqlserver.sql_text,sqlserver.client_hostname,sqlserver.client_app_name,sqlserver.database_name,sqlserver.nt_username,sqlserver.username,sqlserver.server_principal_name)
-    WHERE ([duration]>=(120000000) AND ([package0].[greater_than_uint64]([sqlserver].[database_id],(4))) AND ([sqlserver].[is_system]=(0)))),
+    WHERE ([duration]>=(60000000) AND ([package0].[greater_than_uint64]([sqlserver].[database_id],(4))) AND ([sqlserver].[is_system]=(0)))),
 ADD EVENT sqlserver.sql_statement_completed(SET collect_statement=(1)
     ACTION(sqlserver.query_hash,sqlserver.sql_text,sqlserver.client_hostname,sqlserver.client_app_name,sqlserver.database_name,sqlserver.nt_username,sqlserver.username,sqlserver.server_principal_name)
-    WHERE ([duration]>=(120000000) AND ([package0].[greater_than_uint64]([sqlserver].[database_id],(4))) AND ([sqlserver].[is_system]=(0))))
+    WHERE ([duration]>=(60000000) AND ([package0].[greater_than_uint64]([sqlserver].[database_id],(4))) AND ([sqlserver].[is_system]=(0))))
 ADD TARGET package0.event_file(SET filename=N'E:\ExtendedEvents\QueryMetrics.xel',max_file_size=(128),max_rollover_files=(20))
 WITH (STARTUP_STATE = ON);
 GO
