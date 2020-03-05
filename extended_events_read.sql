0fa9b0bc9043e681787dd6ed17b4e83dc3553487
Alexey Semchenko <asemchenko@outlook.com>
2020-03-05 17:19:07 +0300

    Formatting

--- extended_events_read.sql
+++ extended_events_read.sql
@@ -32,75 +32,80 @@ GO
 */
 
 IF OBJECT_ID('tempdb..#event_data') IS NOT NULL
-	DROP TABLE #event_data;
+    DROP TABLE #event_data;
 SELECT
-	HASHBYTES('SHA2_256'
-		,[file_name]
-		+ N':offset_' + cast(file_offset as nvarchar(20))
-		+ N':data_' + cast(event_data as nvarchar(4000))
-	) AS row_hash
-	,CAST(event_data AS XML) as event_data
+    HASHBYTES('SHA2_256'
+        ,[file_name]
+        + N':offset_' + CAST(file_offset AS NVARCHAR(20))
+        + N':data_' + CAST(event_data AS NVARCHAR(4000))
+    ) AS row_hash
+   ,CAST(event_data AS XML) AS event_data
 INTO #event_data
 FROM
-	sys.fn_xe_file_target_read_file('E:\ExtendedEvents\QueryMetrics_*.xel', null, null, null)
+    sys.fn_xe_file_target_read_file('E:\ExtendedEvents\QueryMetrics_*.xel', NULL, NULL, NULL)
 WHERE
-	[object_name] != N'existing_connection';
+    [object_name] != N'existing_connection';
 
 INSERT INTO msdb.dbo.tracked_queries WITH (TABLOCK) (
-	[timestamp]
-	,duration
-	,event_name
-	,[database_name]
-	,sql_statement
-	,sql_text
-	,[object_name]
-	,row_count
-	,cpu
-	,physical_reads
-	,logical_reads
-	,logical_writes
-	,query_hash
-	,client_hostname
-	,client_app_name
-	,server_principal_name
-	,username
-	,nt_username
-	,row_hash
+    [timestamp]
+   ,duration
+   ,event_name
+   ,[database_name]
+   ,sql_statement
+   ,sql_text
+   ,[object_name]
+   ,row_count
+   ,cpu
+   ,physical_reads
+   ,logical_reads
+   ,logical_writes
+   ,query_hash
+   ,client_hostname
+   ,client_app_name
+   ,server_principal_name
+   ,username
+   ,nt_username
+   ,row_hash
 )
 SELECT
-	DATEADD(HOUR, 3, n.value('(@timestamp)[1]', 'datetime2(3)')) AS [timestamp],
-	CAST(n.value('(data[@name="duration"]/value)[1]', 'bigint') / 60000000. as decimal(19, 1)) as duration,
-	n.value('(@name)[1]', 'nvarchar(255)') as event_name,
-	n.value('(action[@name="database_name"]/value)[1]', 'nvarchar(255)') as [database_name],
-	COALESCE(
-		n.value('(data[@name="statement"]/value)[1]', 'nvarchar(max)')
-		, n.value('(data[@name="batch_text"]/value)[1]', 'nvarchar(max)')
-	) as sql_statement,
-	n.value('(action[@name="sql_text"]/value)[1]', 'nvarchar(max)') as sql_text,
-	n.value('(data[@name="object_name"]/value)[1]', 'nvarchar(255)') as [object_name],
-	n.value('(data[@name="row_count"]/value)[1]', 'bigint') as row_count,
-	n.value('(data[@name="cpu_time"]/value)[1]', 'bigint') as cpu,
-	n.value('(data[@name="physical_reads"]/value)[1]', 'bigint') as physical_reads,
-	n.value('(data[@name="logical_reads"]/value)[1]', 'bigint') as logical_reads,
-	n.value('(data[@name="writes"]/value)[1]', 'bigint') as logical_writes,
-	n.value('(action[@name="query_hash"]/value)[1]', 'decimal(38,0)') as query_hash,
-	n.value('(action[@name="client_hostname"]/value)[1]', 'nvarchar(255)') as client_hostname,
-	n.value('(action[@name="client_app_name"]/value)[1]', 'nvarchar(255)') as client_app_name,
-	n.value('(action[@name="server_principal_name"]/value)[1]', 'nvarchar(255)') as server_principal_name,
-	n.value('(action[@name="username"]/value)[1]', 'nvarchar(255)') as username,
-	n.value('(action[@name="nt_username"]/value)[1]', 'nvarchar(255)') as nt_username,
-	ed.row_hash
+    DATEADD(HOUR, 3, n.value('(@timestamp)[1]', 'datetime2(3)')) AS [timestamp]
+   ,CAST(n.value('(data[@name="duration"]/value)[1]', 'bigint') / 60000000. AS DECIMAL(19, 1)) AS duration
+   ,n.value('(@name)[1]', 'nvarchar(255)') AS event_name
+   ,n.value('(action[@name="database_name"]/value)[1]', 'nvarchar(255)') AS [database_name]
+   ,COALESCE(
+        n.value('(data[@name="statement"]/value)[1]', 'nvarchar(max)')
+        ,n.value('(data[@name="batch_text"]/value)[1]', 'nvarchar(max)')
+    ) AS sql_statement
+   ,n.value('(action[@name="sql_text"]/value)[1]', 'nvarchar(max)') AS sql_text
+   ,n.value('(data[@name="object_name"]/value)[1]', 'nvarchar(255)') AS [object_name]
+   ,n.value('(data[@name="row_count"]/value)[1]', 'bigint') AS row_count
+   ,n.value('(data[@name="cpu_time"]/value)[1]', 'bigint') AS cpu
+   ,n.value('(data[@name="physical_reads"]/value)[1]', 'bigint') AS physical_reads
+   ,n.value('(data[@name="logical_reads"]/value)[1]', 'bigint') AS logical_reads
+   ,n.value('(data[@name="writes"]/value)[1]', 'bigint') AS logical_writes
+   ,n.value('(action[@name="query_hash"]/value)[1]', 'decimal(38,0)') AS query_hash
+   ,n.value('(action[@name="client_hostname"]/value)[1]', 'nvarchar(255)') AS client_hostname
+   ,n.value('(action[@name="client_app_name"]/value)[1]', 'nvarchar(255)') AS client_app_name
+   ,n.value('(action[@name="server_principal_name"]/value)[1]', 'nvarchar(255)') AS server_principal_name
+   ,n.value('(action[@name="username"]/value)[1]', 'nvarchar(255)') AS username
+   ,n.value('(action[@name="nt_username"]/value)[1]', 'nvarchar(255)') AS nt_username
+   ,ed.row_hash
 FROM
-	#event_data AS ed
-	CROSS APPLY ed.event_data.nodes('event') AS q (n)
-WHERE NOT EXISTS (
-	SELECT 1
-	FROM msdb.dbo.tracked_queries AS tq
-	WHERE tq.row_hash = ed.row_hash
-);
+    #event_data AS ed
+    CROSS APPLY ed.event_data.nodes('event') AS q (n)
+WHERE
+    n.value('(action[@name="database_name"]/value)[1]', 'nvarchar(255)') NOT IN (N'master', N'msdb', N'SSISDB')
+    AND NOT EXISTS (
+        SELECT
+            1
+        FROM
+            msdb.dbo.tracked_queries AS tq
+        WHERE
+            tq.row_hash = ed.row_hash
+    );
 
 SELECT
-	*
+    *
 FROM
-	msdb.dbo.tracked_queries
+    msdb.dbo.tracked_queries
 ORDER BY duration DESC;
