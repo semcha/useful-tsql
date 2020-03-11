df7df6e38d29c7926fc4cdd423d784e234e2db38
Alexey Semchenko <asemchenko@outlook.com>
2020-03-11 14:58:25 +0300

    Update sqlmon script

--- sqlmon.sql
+++ sqlmon.sql
@@ -1,31 +1,33 @@
 -- Monitoring running queries
 SELECT
     es.session_id AS [sid]
-   ,er.blocking_session_id AS bloc_sid
-   ,CAST(er.total_elapsed_time / 1000. / 60. AS DECIMAL(34, 2)) AS [duration]
+   ,er.blocking_session_id AS block_sid
+   ,CAST(er.total_elapsed_time / 1000. / 60. AS decimal(34, 2)) AS [duration]
+   ,mg.dop
+   ,mg.granted_memory_kb / 1024 AS ram_mb
+   ,CAST(mg.query_cost AS int) AS qry_cost
+   ,er.[status]
    ,SUBSTRING(qt.[text], (er.statement_start_offset / 2) + 1,
     ((CASE
-        WHEN er.statement_end_offset = -1 THEN LEN(CONVERT(NVARCHAR(MAX), qt.[text])) * 2
+        WHEN er.statement_end_offset = -1 THEN LEN(CONVERT(nvarchar(max), qt.[text])) * 2
         ELSE er.statement_end_offset
     END - er.statement_start_offset) / 2) + 1) AS [statement]
    ,qt.[text] AS query
    ,DB_NAME(er.database_id) AS [db_name]
    ,OBJECT_SCHEMA_NAME(qt.objectid, er.database_id) + N'.' + OBJECT_NAME(qt.objectid, er.database_id) AS [object_name]
-   ,er.[status]
-   ,er.command
+   ,er.wait_type + N': ' + CAST(er.wait_time / 1000 AS nvarchar(50)) + N's' AS wait_type
    ,es.login_name
    ,es.[host_name]
    ,es.[program_name]
    ,er.start_time
-    --,qp.query_plan
-   ,er.wait_type
+   ,qp.query_plan
    ,er.cpu_time
+   ,er.reads AS physical_reads
    ,er.logical_reads
+   ,er.writes AS physical_writes
    ,er.open_transaction_count
    ,er.last_wait_type
    ,er.percent_complete
-   ,mg.requested_memory_kb / 1024 AS requested_memory_mb
-   ,mg.granted_memory_kb / 1024 AS granted_memory_mb
 FROM
     sys.dm_exec_requests AS er WITH (NOLOCK)
     INNER JOIN sys.dm_exec_sessions AS es WITH (NOLOCK)
@@ -33,9 +35,9 @@ FROM
     LEFT JOIN sys.dm_exec_query_memory_grants AS mg WITH (NOLOCK)
         ON es.session_id = mg.session_id
     CROSS APPLY sys.dm_exec_sql_text(er.[sql_handle]) AS qt
---OUTER APPLY sys.dm_exec_query_plan(er.plan_handle) AS qp
+    OUTER APPLY sys.dm_exec_query_plan(er.plan_handle) AS qp
 WHERE
     es.is_user_process = 1
     AND es.session_id NOT IN (@@spid)
-ORDER BY [statement], er.session_id DESC
+ORDER BY er.session_id
 OPTION (RECOMPILE);
