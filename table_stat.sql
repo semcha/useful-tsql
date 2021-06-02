02b4789c449518c9cf68c6ec67ae5ef2f5cdb28b
Alexey <asemchenko@outlook.com>
2021-06-03 00:17:48 +0300

    Update table_stat.sql

--- table_stat.sql
+++ table_stat.sql
@@ -1,90 +1,90 @@
-WITH table_clustering AS (
-    SELECT
+﻿with table_clustering as (
+    select
         ix.[object_id]
        ,ix.index_id
-       ,CASE
-            WHEN ix.is_primary_key = 1 THEN ix.[type_desc] + N' PK'
-            WHEN ix.is_unique = 1 THEN ix.[type_desc] + N' UNIQUE'
-            WHEN ix.is_disabled = 1 THEN N'!!! DISABLED ' + ix.[type_desc]
-            ELSE ix.[type_desc]
-        END AS clustering_type
-    FROM
-        sys.indexes AS ix
-    WHERE
-        ix.[type] IN (0, 1, 5) -- HEAP, CLUSTERED B-tree/Columnstore
+       ,case
+            when ix.is_primary_key = 1 then ix.[type_desc] + N' PK'
+            when ix.is_unique = 1 then ix.[type_desc] + N' UNIQUE'
+            when ix.is_disabled = 1 then N'!!! DISABLED ' + ix.[type_desc]
+            else ix.[type_desc]
+        end as table_organization
+    from
+        sys.indexes as ix with (nolock)
+    where
+        ix.[type] in (0, 1, 5) -- HEAP, CLUSTERED B-tree/Columnstore
 ),
-table_partitions AS (
-    SELECT
+table_partitions as (
+    select
         p.[object_id]
        ,p.index_id
-       ,COUNT(1) AS partitions_count
-       ,SUM(p.[rows]) AS rows_count
-       ,MIN(p.data_compression_desc) AS [compression]
-       ,COUNT(IIF(p.[data_compression] IS NOT NULL, 1, NULL)) AS compressed_partitions
-    FROM
-        sys.partitions AS p
-    GROUP BY
+       ,count(1) as partitions_count
+       ,sum(p.[rows]) as rows_count
+       ,min(p.data_compression_desc) as [compression]
+       ,count(iif(p.[data_compression] > 0, 1, null)) as compressed_partitions
+    from
+        sys.partitions as p with (nolock)
+    group by
         p.[object_id]
        ,p.index_id
 ),
-table_space AS (
-    SELECT
+table_space as (
+    select
         s.[object_id]
-       ,CAST(SUM(s.[used_page_count]) * 8. / 1024. / 1024. AS decimal(19, 2)) AS reserved_gb
-       ,CAST(SUM(IIF(ix.[type] IN (0, 1, 5), s.[used_page_count], 0)) * 8. / 1024. / 1024. AS decimal(19, 2)) AS table_gb
-       ,CAST(SUM(IIF(ix.[type] IN (2, 6, 7), s.[used_page_count], 0)) * 8. / 1024. / 1024. AS decimal(19, 2)) AS indexes_gb
-    FROM
-        sys.dm_db_partition_stats AS s
-        INNER JOIN sys.indexes AS ix
-            ON s.[object_id] = ix.[object_id]
-                AND s.index_id = ix.index_id
-    GROUP BY
+       ,cast(sum(s.[used_page_count]) * 8. / 1024. / 1024. as decimal(19, 2)) as reserved_gb
+       ,cast(sum(iif(ix.[type] in (0, 1, 5), s.[used_page_count], 0)) * 8. / 1024. / 1024. as decimal(19, 2)) as table_gb
+       ,cast(sum(iif(ix.[type] in (2, 6, 7), s.[used_page_count], 0)) * 8. / 1024. / 1024. as decimal(19, 2)) as indexes_gb
+    from
+        sys.dm_db_partition_stats as s with (nolock)
+        inner join sys.indexes as ix with (nolock)
+            on s.[object_id] = ix.[object_id]
+                and s.index_id = ix.index_id
+    group by
         s.[object_id]
 ),
-nc_indexes_source AS (
-    SELECT
+nc_indexes_source as (
+    select
         qryix.[object_id]
-       ,LTRIM(
+       ,ltrim(
         qryix.index_is_disabled
         + N' ' + qryix.index_has_filter
         + N' ' + qryix.index_type_1
         + N' ' + qryix.index_type_2
-        + N': ' + CAST(qryix.index_count AS nvarchar(50))
-        ) AS index_type
-    FROM
+        + N': ' + cast(qryix.index_count as nvarchar(50))
+        ) as index_type
+    from
         (
-            SELECT
+            select
                 ix.[object_id]
                ,ix.[type]
                ,ix.is_unique
                ,ix.is_primary_key
                ,ix.is_unique_constraint
                ,ix.is_disabled
-               ,COUNT(1) AS index_count
-               ,CASE
-                    WHEN ix.is_disabled = 1 THEN N'FILTERED'
-                    ELSE N''
-                END AS index_is_disabled
-               ,CASE
-                    WHEN ix.has_filter = 1 THEN N'FILTERED'
-                    ELSE N''
-                END AS index_has_filter
-               ,CASE
-                    WHEN ix.[type] = 2 THEN N'NC B-TREE'
-                    WHEN ix.[type] = 6 THEN N'NC COLUMNSTORE'
-                    WHEN ix.[type] = 7 THEN N'HASH'
-                END index_type_1
-               ,CASE
-                    WHEN ix.is_primary_key = 1 THEN N'PK'
-                    WHEN ix.is_unique_constraint = 1 THEN N'UQ CNSTRNT'
-                    WHEN ix.is_unique = 1 THEN N'UQ'
-                    ELSE N''
-                END index_type_2
-            FROM
-                sys.indexes AS ix
-            WHERE
-                ix.[type] IN (2, 6, 7) -- NONCLUSTERED B-tree/Columnstore, HASH
-            GROUP BY
+               ,count(1) as index_count
+               ,case
+                    when ix.is_disabled = 1 then N'!!! DISABLED'
+                    else N''
+                end as index_is_disabled
+               ,case
+                    when ix.has_filter = 1 then N'FILTERED'
+                    else N''
+                end as index_has_filter
+               ,case
+                    when ix.[type] = 2 then N'NC B-TREE'
+                    when ix.[type] = 6 then N'NC COLUMNSTORE'
+                    when ix.[type] = 7 then N'HASH'
+                end index_type_1
+               ,case
+                    when ix.is_primary_key = 1 then N'PK'
+                    when ix.is_unique_constraint = 1 then N'UQ CNSTRNT'
+                    when ix.is_unique = 1 then N'UQ'
+                    else N''
+                end index_type_2
+            from
+                sys.indexes as ix with (nolock)
+            where
+                ix.[type] in (2, 6, 7) -- NONCLUSTERED B-tree/Columnstore, HASH
+            group by
                 ix.[object_id]
                ,ix.[type]
                ,ix.is_unique
@@ -92,36 +92,36 @@ nc_indexes_source AS (
                ,ix.is_unique_constraint
                ,ix.is_disabled
                ,ix.has_filter
-        ) AS qryix
+        ) as qryix
 ),
-nc_indexes AS (
-    SELECT
+nc_indexes as (
+    select
         ncix.[object_id]
-       ,N'{' + STUFF(CAST((
-            SELECT
+       ,N'{' + stuff(cast((
+            select
                 [text()] = '; ' + index_type
-            FROM
-                nc_indexes_source AS qry
-            WHERE
+            from
+                nc_indexes_source as qry
+            where
                 qry.[object_id] = ncix.[object_id]
-            FOR xml PATH (''), TYPE
+            for xml path (''), type
         )
-        AS nvarchar(max))
-        , 1, 1, N'') + N'}' AS nc_indexes
-    FROM
+        as nvarchar(max))
+        , 1, 1, N'') + N'}' as nc_indexes
+    from
         (
-            SELECT DISTINCT
+            select distinct
                 [object_id]
-            FROM
+            from
                 nc_indexes_source
-        ) AS ncix
+        ) as ncix
 )
-SELECT
-    DB_NAME() AS [database_name]
+select
+    db_name() as [database_name]
    ,t.[object_id]
-   ,OBJECT_SCHEMA_NAME(t.[object_id]) + N'.' + t.[name] AS table_name
-   ,t.is_memory_optimized AS is_inmem
-   ,tblcls.clustering_type
+   ,object_schema_name(t.[object_id]) + N'.' + t.[name] as table_name
+   ,t.is_memory_optimized as is_inmem
+   ,tblcls.table_organization
    ,tblprttn.partitions_count
    ,tblprttn.compressed_partitions
    ,tblprttn.[compression]
@@ -130,17 +130,53 @@ SELECT
    ,tblspc.table_gb
    ,tblspc.indexes_gb
    ,ncix.nc_indexes
-FROM
-    sys.tables AS t
-    INNER JOIN table_clustering AS tblcls
-        ON t.[object_id] = tblcls.[object_id]
-    INNER JOIN table_partitions AS tblprttn
-        ON t.[object_id] = tblprttn.[object_id]
-            AND tblcls.index_id = tblprttn.index_id
-    INNER JOIN table_space AS tblspc
-        ON t.[object_id] = tblspc.[object_id]
-    LEFT JOIN nc_indexes AS ncix
-        ON t.[object_id] = ncix.[object_id]
-WHERE
+from
+    sys.tables as t with (nolock)
+    inner join table_clustering as tblcls
+        on t.[object_id] = tblcls.[object_id]
+    inner join table_partitions as tblprttn
+        on t.[object_id] = tblprttn.[object_id]
+            and tblcls.index_id = tblprttn.index_id
+    inner join table_space as tblspc
+        on t.[object_id] = tblspc.[object_id]
+    left join nc_indexes as ncix
+        on t.[object_id] = ncix.[object_id]
+where
     t.[type] = N'U'
-ORDER BY tblspc.reserved_gb DESC;
+order by tblspc.reserved_gb desc;
+
+select
+    object_schema_name(t.[object_id]) + N'.' + t.[name] as table_name
+   ,ix.[type_desc] as index_type
+   ,ix.[name] as index_name
+   ,ps.[name] as p_scheme
+   ,pf.[name] as p_function
+   ,col.[name] as p_column
+   ,prttnvl.p_function_min_value
+   ,prttnvl.p_function_max_value
+from
+    sys.tables as t with (nolock)
+    inner join sys.indexes as ix with (nolock)
+        on t.object_id = ix.object_id
+    inner join sys.partition_schemes as ps with (nolock)
+        on ix.data_space_id = ps.data_space_id
+    inner join sys.partition_functions as pf with (nolock)
+        on ps.function_id = pf.function_id
+    inner join sys.index_columns as ixcol with (nolock)
+        on ix.[object_id] = ixcol.[object_id]
+            and ix.index_id = ixcol.index_id
+            and ixcol.partition_ordinal > 0
+    inner join sys.columns as col with (nolock)
+        on ixcol.[object_id] = col.[object_id]
+            and ixcol.column_id = col.column_id
+    inner join (
+        select
+            prv.function_id
+           ,min(prv.[value]) as p_function_min_value
+           ,max(prv.[value]) as p_function_max_value
+        from
+            sys.partition_range_values as prv with (nolock)
+        group by
+            prv.function_id
+    ) as prttnvl
+        on pf.function_id = prttnvl.function_id;
